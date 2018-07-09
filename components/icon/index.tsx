5b7798f4d055eb5d5e6c314a45a2e14f64997678
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-07-09 18:07:31 +0800

    new icon api

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -7,6 +7,34 @@ import { library, antDesignIcons } from "antd-icons/esm";
 library.add(...antDesignIcons);
 
 export interface IconProps {
+  type?: string;
+  className?: string;
+  title?: string;
+  onClick?: React.MouseEventHandler<any>;
+  spin?: boolean;
+  style?: React.CSSProperties;
+}
+
+const Icon: React.SFC<IconProps> = (props) => {
+  const { type, className = '', spin } = props;
+  const classString = classNames({
+    anticon: true,
+    'anticon-spin': !!spin || type === 'loading'
+  }, className);
+
+  const content = type ? <AntdIcon type={type}/> : (
+    <svg width={'1em'} height={'1em'} fill={'currentColor'}>
+      {props.children}
+    </svg>
+  );
+  return (
+    <i className={classString} {...omit(props, ['type', 'spin'])}>
+      {content}
+    </i>
+  );
+};
+
+export interface CustomIconProps {
   type: string;
   className?: string;
   title?: string;
@@ -15,15 +43,37 @@ export interface IconProps {
   style?: React.CSSProperties;
 }
 
-const Icon = (props: IconProps) => {
-  const { type, className = '', spin } = props;
-  const classString = classNames({
-    anticon: true,
-    'anticon-spin': !!spin || type === 'loading'
-  }, className);
-  return (
-    <AntdIcon type={type} className={classString} {...omit(this.props, ['type', 'spin'])}/>
-  );
-};
+export interface CustomIconOptions {
+  namespace: string;
+  prefix?: string;
+  scriptLink?: string;
+}
+
+(Icon as any).create = create;
+
+const customIconsCache: { [namespace: string]: boolean } = {};
+
+function create({ namespace, prefix, scriptLink }: CustomIconOptions) {
+  return class CustomIcon extends React.Component<CustomIconProps> {
+
+    componentDidMount() {
+      if (document && typeof document.createElement === 'function' && !customIconsCache[namespace] && scriptLink) {
+        const script = document.createElement('script');
+        script.src = scriptLink;
+        customIconsCache[namespace] = true;
+        document.body.appendChild(script);
+      }
+    }
+
+    render() {
+      const { type, ...rest } = this.props;
+      return (
+        <Icon {...rest}>
+          <use xlinkHref={`#${prefix}${type}`}/>
+        </Icon>
+      );
+    }
+  };
+}
 
 export default Icon;
